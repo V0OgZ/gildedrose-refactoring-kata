2d76b05d7dec07d2c43bac37b64eda71c9250a5f
Vincent <vincent@Vincents-Mac-mini.local>
2026-04-24 19:53:05 +0200

    refactor: initialize text fixture with inventory items

--- Java/src/main/java/com/gildedrose/InventoryItem.java
+++ Java/src/main/java/com/gildedrose/InventoryItem.java
@@ -1,9 +1,21 @@
 package com.gildedrose;
 
-// Associates a legacy Item with the update function 
+// Associates a legacy Item with the update function.
 final class InventoryItem {
     final Item item;
     final ItemUpdate update;
+
+    InventoryItem(String name, int sellIn, int quality) {
+        this(new Item(name, sellIn, quality));
+    }
+
+    InventoryItem(String name, int sellIn, int quality, ItemUpdate update) {
+        this(new Item(name, sellIn, quality), update);
+    }
+
+    InventoryItem(Item item) {
+        this(item, itemToUpdate -> {});
+    }
 
     InventoryItem(Item item, ItemUpdate update) {
         this.item = item;
